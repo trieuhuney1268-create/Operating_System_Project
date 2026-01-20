63c8d725553f1039c7b0b71bfd6d8ef8ceef30db
trieuhuney1268-create <trieuhuney1268@gmail.com>
2026-01-20 09:08:15 +0700

    Update IO.h

--- src/IO.h
+++ src/IO.h
@@ -1,4 +1,15 @@
 #ifndef IO_H
 #define IO_H
+#include <string>
+#include "list_process.h"
+using namespace std;
+
+class IO
+{
+    public:
+            static void readCSV(const string& filename, list_process& L);
+            static void save_to_file(const string& filename, const std::vector<Process>& result_prcs);
+            static vector<Process> load_file(const string& filename);
+};
 
 #endif // IO_H
